ce1a7d765c2d95f9a421514284d9bc10fe8ad291
Alexsander Oliveira <alexsander1996@yahoo.com.br>
2021-03-21 21:23:38 -0300

    Início do CountdownContext
    Iniciado a criação do CountdownContext com algumas alterações do código para melhor funcionamento geral.

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/Alexz96.png" alt="Alexsander Oliveira" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Alexsander Oliveira</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/global.css';
 
 import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-
 function MyApp({ Component, pageProps }) {
 
   return (
